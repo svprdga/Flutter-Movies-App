ca4c2eaa83a0d9f7e94b79d46d228b741983da9d
David Serrano <david@davidserrano.io>
2023-07-29 13:08:30 +0200

    Add PreferencesRepository tests

--- test/mocks.dart
+++ test/mocks.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_movies_app/data/database/dao/movies_dao.dart';
 import 'package:flutter_movies_app/data/database/database_mapper.dart';
+import 'package:flutter_movies_app/data/datasource/preferences.dart';
 import 'package:flutter_movies_app/data/network/client/api_client.dart';
 import 'package:flutter_movies_app/data/network/network_mapper.dart';
 import 'package:mocktail/mocktail.dart';
@@ -11,3 +12,5 @@ class NetworkMapperMock extends Mock implements NetworkMapper {}
 class MoviesDaoMock extends Mock implements MoviesDao {}
 
 class DatabaseMapperMock extends Mock implements DatabaseMapper {}
+
+class PreferencesMock extends Mock implements Preferences {}
